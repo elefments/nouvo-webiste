b8f77516064e581956c03b9858729747bbdcc0b4
Lefos <lefpoge@gmail.com>
2026-05-27 18:29:32 +0300

    Remove deprecated middleware.ts (replaced by proxy.ts for Next.js 16)

--- middleware.ts
+++
@@ -1,8 +0,0 @@
-import createMiddleware from 'next-intl/middleware'
-import { routing } from './src/i18n/routing'
-
-export default createMiddleware(routing)
-
-export const config = {
-  matcher: ['/((?!api|_next|admin|media|fonts|favicon.ico|favicon.svg|sitemap.xml|robots.txt).*)'],
-}
